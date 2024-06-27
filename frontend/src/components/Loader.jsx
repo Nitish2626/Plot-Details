5f161b4c4dd42468a0dc63307bdb876c87409514
Nitish Kumar Sharma <133589058+Nitish2626@users.noreply.github.com>
2024-06-27 19:33:30 +0530

    Delete frontend directory

--- frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-const Loader = () => {
-    return (
-        <div className="w-12 h-12 animate-spin rounded-full border-white border-t-2 border-r-2">
-        </div>
-    );
-};
-
-export default Loader;
